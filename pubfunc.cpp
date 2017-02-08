6bcb9df2a325da3db8a54ee17b2d04defd86d328
unknown <wangkai17534@hs.handsome.com.cn>
2017-02-08 18:24:26 +0800

    优化

--- pubfunc.cpp
+++ pubfunc.cpp
@@ -1,19 +1,11 @@
 #include "pubfunc.h"
 
-
-
 const char *toCStr(const QString &qStr)
 {
    static string str ;
    str = qStr.toLocal8Bit().toStdString();
    return str.c_str() ;
 }
-
-//const char *toCStr(const char *str)
-//{
-//   return str ;
-//}
-
 
 QString toQStr(const string &str)
 {
@@ -30,23 +22,19 @@ const char *toCStr(const string &str)
 
 void readPtreeFromUtf8(const string &fileName, boost::property_tree::ptree &pt)
 { 
+   // 读取源文件，将UTF8->GBK，再放入property_tree
    pt.clear();
    ifstream ifs(fileName) ;
    if ( !ifs ) {
       return ;
    }
-   ifs.seekg(0, ios::end) ;
-   size_t filesize = ifs.tellg();
-   char *p = new char[filesize+1] ;
-   boost::shared_array<char> pArray(p) ;
-   
-   memset(pArray.get(), 0, filesize+1) ;
-   ifs.seekg(0, ios::beg);
-   ifs.read(pArray.get(), filesize) ;
-   auto outstr = boost::locale::conv::between(pArray.get(),"gbk", "utf8") ;
-
-   std::stringstream iostr(outstr) ;
-   
+   string strText ;
+   std::copy( std::istreambuf_iterator<char>(ifs),
+              std::istreambuf_iterator<char>(),
+              std::back_inserter(strText)) ;
+   ifs.close();
+   strText = boost::locale::conv::between(strText, "gbk", "utf8") ;
+   std::stringstream iostr(strText) ;
    boost::property_tree::read_xml(iostr, pt);
    
 }
